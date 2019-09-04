fbc1c91b571261f55478fab111e4afa437069669
bertt <bert.temme@geodan.nl>
2019-09-04 13:06:34 +0200

    add initial draw sphere for glTF

---
+++ GenerateGlTF/GenerateGlTF/GenerateGlTF/MeshBuilderExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿using SharpGLTF.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace GenerateGlTF
+{
+    public static class MeshBuilderExtensionMethods
+    {
+        public static void AddSphere<TMaterial>(this IMeshBuilder<TMaterial> meshBuilder, TMaterial material, Single radius, Matrix4x4 xform)
+        {
+            var sphere = new IcoSphere<TMaterial>(material, radius);
+
+            sphere.AddTo(meshBuilder, xform);
+        }
+
+    }
+}
---
+++ GenerateGlTF/GenerateGlTF/GenerateGlTF/Program.cs
@@ -0,0 +1,48 @@
+﻿using GeoJSON.Net.Feature;
+using GeoJSON.Net.Geometry;
+using Newtonsoft.Json;
+using SharpGLTF.Geometry;
+using SharpGLTF.Geometry.VertexTypes;
+using SharpGLTF.Materials;
+using SharpGLTF.Scenes;
+using System;
+using System.IO;
+using System.Numerics;
+
+namespace GenerateGlTF
+{
+    using VPOSNRM = VertexBuilder<VertexPositionNormal, VertexEmpty, VertexEmpty>;
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Les create some glTF with spheres to test.");
+            // read points.geojson
+            var json = File.ReadAllText("Points.geojson");
+            var fc = JsonConvert.DeserializeObject<FeatureCollection>(json);
+            Console.WriteLine("Features: " + fc.Features.Count);
+
+            foreach(var f in fc.Features)
+            {
+                var z = f.Properties["z"];
+                var timestamp = f.Properties["timestamp"];
+                var amount = f.Properties["amount"];
+
+                var point = (Point)f.Geometry;
+                var coords = point.Coordinates;
+                Console.WriteLine(timestamp + ": " + coords.Longitude + ", " + coords.Latitude + ", " + z + ", " + amount);
+
+                // todo: draw sphere per feature
+            }
+
+            var material = MaterialBuilder.CreateDefault();
+            var mesh = VPOSNRM.CreateCompatibleMesh("shape");
+            mesh.AddSphere(material, 0.5f, Matrix4x4.Identity);
+            var scene = new SceneBuilder();
+            scene.AddMesh(mesh, Matrix4x4.Identity);
+            scene.ToSchema2().SaveGLB("sphere.glb");
+        }
+    }
+}
